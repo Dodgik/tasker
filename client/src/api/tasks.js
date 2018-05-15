76253e18d2b37f9b612d73356cf91ff064f70948
Vasiliy <vasiliy017@gmail.com>
2018-05-15 11:10:10 +0300

    remove unused
    Signed-off-by: Vasiliy <vasiliy017@gmail.com>

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -1,15 +1,7 @@
-import { call, select } from 'redux-saga/effects'
 import fetch from 'isomorphic-fetch'
 import queryString from 'query-string'
 import MD5 from 'md5.js'
 
-const selectApiParams = state => state.api
-
-function* getApiParams() {
-  const apiParams = yield select(selectApiParams);
-
-  return apiParams;
-}
 
 const getProps = {
   method: 'GET',
